1a9a7d73cb66893e2dfdb45909f87d7a74421656
Alejandro <alejodiaz.123@gmail.com>
2020-04-02 21:13:32 -0300

    Proyecto finalizado

---
+++ app/models/book.rb
@@ -0,0 +1,6 @@
+class Book < ApplicationRecord
+    validates :name, presence: true
+    validates :author, presence: true
+    validates :state, presence: true
+    enum state:[:prestado, :en_estante]
+end
